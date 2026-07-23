4d5f59ccb0fa9d896b37662997bcbd9662226408
EMIC Compiler <emic-compiler@emic.io>
2026-07-23 21:35:43 +0000

    Code generation: 2026-07-23 21:35:43 UTC
    Errors: 0
    Warnings: 0
    
    ---
    Module: boot
    Project: boot-build
    User: lugaro95-at-gmail-com

--- inc/boot_id.h
+++ inc/boot_id.h
@@ -1,7 +1,7 @@
 #ifndef BOOT_ID_H
 #define BOOT_ID_H
 
-#define BOOT_MODULE_ID      { 'I','A','C','A','A','B' }
+#define BOOT_MODULE_ID      { 'I','A','C','A','A','C' }
 #define BOOT_MODULE_ID_LEN  6
 
 #endif /* BOOT_ID_H */
